0d8550f4141d8846ac54742544675dab24fadcf2
Tracie35 <kariukitracie@gmail.com>
2022-07-21 07:41:57 +0300

    done

--- src/__tests__/Filter.test.js
+++ src/__tests__/Filter.test.js
@@ -1,6 +1,6 @@
 import "@testing-library/jest-dom";
 import { render, screen, fireEvent } from "@testing-library/react";
-import Filter from "../components/Filter";
+import Header from "../components/Filter";
 
 test("displays the <select> element", () => {
   render(<Filter />);
